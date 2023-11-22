34327de85263fdf043db2bef6d9c12c0612747e2
Jesús Guijarro <72225659+JesusEduardoGuijarroSaldana@users.noreply.github.com>
2023-11-22 08:05:20 -0600

    Ejercicio 09

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,25 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'CBDF_JEGS_09';
+  title = 'Ejercicio 09 - Interpolación';
+  name = 'Jóse Antonio Vargas López';
+  age = 24;
+  semester = 'Quinto semestre';
+  grades = [8, 7, 10, 10, 8, 9, 8, 6];
+  mean = this.calculateMean();
+  status = this.approved();
+  calculateMean() {
+    let sum = 0;
+    const length = this.grades.length;
+    for (let i = 0; i < length; i++) sum += this.grades[i];
+    return sum / length;
+  }
+
+  approved() {
+    if (this.calculateMean() >= 6) return 'Aprobado';
+    else return 'No aprobado';
+  }
 }
